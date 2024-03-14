03ca7ae2075e0676f700eb7689c18a578c854a6f
pawan sen <aymanjain007@example.com>
2024-03-14 14:32:50 +0530

    home screen on display dynaminc logo

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,10 +5,21 @@ import { Image } from 'react-bootstrap';
 import './Home.css'
 import BarmouLogo from '../../assets/images/barmou-logo.png'
 import PlateLogo from '../../assets/images/plate-fastfood.png'
+import { useSelector, useDispatch } from "react-redux";
+import {
+  getAssetInfo as onGetAssetInfo,
+} from "../../slices/thunks";
 
 const Home = () => {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
   const [assetId, setAssetId] = useState();
+  const [assetInfo, setAssetInfo] = useState({});
+
+  const { asset } =
+    useSelector((state) => ({
+      asset: state.assets.asset
+    }));
 
   useEffect(() => {
     const currentUrl = window.location.href;
@@ -18,7 +29,19 @@ const Home = () => {
     if (assetId !== localStorage.getItem("assetId")) {
       localStorage.setItem("assetId", assetId);
     }
-  }, [])
+    if (assetId)
+      dispatch(onGetAssetInfo({ assetId: `${assetId}` }));
+  }, [dispatch])
+
+  setTimeout(function () {
+    if (asset.length > 0) {
+      setAssetInfo(asset[0]);
+      localStorage.setItem("assetInfoData", JSON.stringify(asset[0]));
+    }
+    if(!assetInfo){
+      setAssetInfo(JSON.parse(assetInfo));
+    }
+  }, 2000);
 
   const handleDineInClick = () => {
     navigate(`/menu?assetId=${assetId}`);
@@ -30,30 +53,30 @@ const Home = () => {
 
   return (
     <>
-    <div className='home-main-container text-light p-5 position-relative'>
+      <div className='home-main-container text-light p-5 position-relative'>
         <div className="d-flex align-items-center">
-            <div className="select-language-icon rounded-circle overflow-hidden  _cursor-pointer">
-                <Image className="_obj-fit-cover h-100 w-100" src='https://source.unsplash.com/random/?flag' />
-            </div>
-            <IoChevronDownSharp className="header-gray mx-1" />
+          <div className="select-language-icon rounded-circle overflow-hidden  _cursor-pointer">
+            <Image className="_obj-fit-cover h-100 w-100" src='https://source.unsplash.com/random/?flag' />
+          </div>
+          <IoChevronDownSharp className="header-gray mx-1" />
         </div>
         <div className='barmou-logo-container text-center'>
-            <Image className="h-100 cursor_pointer" src={BarmouLogo} />
+          <Image className="h-100 cursor_pointer" src={assetInfo.floorPlanImage} />
         </div>
         <h1>€<strong>3,00</strong></h1>
         <h4><strong>EAT & COFFEE</strong></h4>
         <div className='d-flex my-4'>
-            <button onClick={handleDineInClick} className='border-0 p-3 rounded-pill'><strong>Dine in</strong></button>
-            <button onClick={handleTakeAwayClick} className='border-0 p-3 rounded-pill mx-3'><strong>Takeaway</strong></button>
+          <button onClick={handleDineInClick} className='border-0 p-3 rounded-pill'><strong>Dine in</strong></button>
+          <button onClick={handleTakeAwayClick} className='border-0 p-3 rounded-pill mx-3'><strong>Takeaway</strong></button>
         </div>
         <h4>
-            <strong>1 Freddo Espresso</strong>
+          <strong>1 Freddo Espresso</strong>
         </h4>
         <h4>
-            <strong>1 bagel</strong>
+          <strong>1 bagel</strong>
         </h4>
         <Image onClick={handleDineInClick} className='home-bg-food-image position-absolute cursor_pointer' src={PlateLogo} />
-    </div>
+      </div>
     </>
   )
 }
